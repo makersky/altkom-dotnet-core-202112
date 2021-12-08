61d0d3514b0e2383f111bc83ffffaa0608cb8676
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-12-08 11:02:29 +0100

    Add project files.

---
+++ Altkom.Shopper.WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Altkom.Shopper.WebApi.Controllers
+{
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet("api/ping")]
+        public string Ping()
+        {
+            return "Pong";
+        }
+
+        // GET api/customers
+
+    }
+}
