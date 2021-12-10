07ebae2072567a836b0495d8e3d785f05f91c2ba
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-12-10 15:33:51 +0100

    Dodanie Worker Service

---
+++ Alkom.Shopper.WorkerService/CustomerWorkerService.cs
@@ -0,0 +1,36 @@
+﻿using Altkom.Shopper.Models;
+using Bogus;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Alkom.Shopper.WorkerService
+{
+    public class CustomerWorkerService : BackgroundService
+    {
+        private readonly ILogger<CustomerWorkerService> logger;
+        private readonly Faker<Customer> customerFaker;
+
+        public CustomerWorkerService(
+            ILogger<CustomerWorkerService> logger, 
+            Faker<Customer> customerFaker)
+        {
+            this.logger = logger;
+            this.customerFaker = customerFaker;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {          
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var customer = customerFaker.Generate();
+
+                logger.LogInformation($"{customer.FirstName} {customer.LastName}");
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
